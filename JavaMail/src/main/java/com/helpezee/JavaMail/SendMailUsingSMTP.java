4a469886735760321bd6457a60a4aa529ec27739
satya <satya.iton@gmail.com>
2022-01-07 14:36:33 +0530

    Sending mail with SMTP and SMTPS using java mail library

---
+++ JavaMail/src/main/java/com/helpezee/JavaMail/SendMailUsingSMTP.java
@@ -0,0 +1,49 @@
+package com.helpezee.JavaMail;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class SendMailUsingSMTP {
+	public static void main(String[] args) {
+
+		String host = "smtp.gmail.com";
+		final String user = "abc@gmail.com";// change accordingly
+		final String password = "password";// change accordingly
+
+		String to = "xyz@gmail.com";// change accordingly
+
+		Properties props = new Properties();
+
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", 587);
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.starttls.required", "true");
+
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(user, password);
+			}
+		});
+		session.setDebug(true);
+
+		// Compose the message
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(user));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			message.setSubject("TEST MAIL USING SMTP");
+			message.setText("HELLO");
+
+			// send the message
+			Transport.send(message);
+			System.out.println("message sent successfully using smpt");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ JavaMail/src/main/java/com/helpezee/JavaMail/SendMailUsingSMTPS.java
@@ -0,0 +1,45 @@
+package com.helpezee.JavaMail;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class SendMailUsingSMTPS {
+	public static void main(String[] args) {
+
+		String host = "smtp.gmail.com";
+		final String user = "abc@gmail.com";// change accordingly
+		final String password = "password";// change accordingly
+
+		String to = "xyz@gmail.com";// change accordingly
+
+		Properties props = new Properties();
+		props.put("mail.transport.protocol", "smtps");
+		props.put("mail.smtps.host", host);
+		props.put("mail.smtps.port", 465);
+		props.put("mail.smtps.auth", "true");
+		props.put("mail.smtps.starttls.enable", "true");
+		props.put("mail.smtps.starttls.required", "true");
+		Session mailSession = Session.getDefaultInstance(props);
+		mailSession.setDebug(true);
+
+		// Compose the message
+		try {
+			Transport transport = mailSession.getTransport();
+			MimeMessage message = new MimeMessage(mailSession);
+			message.setFrom(new InternetAddress(user));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			message.setSubject("TEST MAIL USING SMTPS");
+			message.setText("HELLO");
+
+			transport.connect(host, 465, user, password);
+
+			transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
+
+			System.out.println("message sent successfully using smpts");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
